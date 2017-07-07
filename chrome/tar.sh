1b241eb9aaf4ebcdd421d70791d885ab81d1d442
root <root@orcas.lan>
2017-07-07 16:46:54 +0200

    bugfix

--- chrome/tar.sh
+++ chrome/tar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cp app smarterPowerPoint
-rm smarterPowerPoint-1.0.0.tar.gz
+cp -r app smarterPowerPoint
+rm -f smarterPowerPoint-1.0.0.tar.gz
 tar -czvf smarterPowerPoint-1.0.0.tar.gz smarterPowerPoint
 rm -r smarterPowerPoint
-cp -f smarterPowerPoint-1.0.0.tar.gz ../service/
+mv -f smarterPowerPoint-1.0.0.tar.gz ../service/
